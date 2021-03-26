c5403944747deca34da9351bfe3fbfb2a6f18967
Pranav Ballaney <ballaneypranav@gmail.com>
2021-03-26 16:13:11 +0530

    Print header and board functions

---
+++ tic-tac-toe.R
@@ -0,0 +1,31 @@
+board = array(rep(" ", 9), dim = c(3, 3))
+
+print_header <- function(round) {
+  cat("#################\n\n")
+  cat("#### Round", round, "####\n\n")
+  cat("#################\n\n")
+}
+
+print_board <- function() {
+  # Prints the current state of the board
+  cat("Current board:\n")
+  cat("~~~~~~~~~~~~~~\n\n")
+  cat("     ")
+  for (i in c(1, 2, 3)) {
+    cat(i, " ")
+  }
+  cat("\n")
+
+  for (i in c(1, 2, 3)) {
+    cat(" ", i, " ")
+    for (c in board[i, ]) {
+      cat(c, " ")
+    }
+    cat("\n")
+  }
+
+  cat("\n~~~~~~~~~~~~~~")
+}
+
+print_header(1)
+print_board()
